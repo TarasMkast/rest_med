355d27a1a8b6fb3f94ce025c15e5d7042e909412
TarasMkast <tarasmotalo@gmail.com>
2020-12-22 11:18:03 +0200

    add permissions

--- about/views.py
+++ about/views.py
@@ -1,22 +1,21 @@
 from .models import Clinic, Doctor, Gallery
-
-from rest_framework import viewsets
-
-
+from rest_framework import viewsets, permissions
 from .serializer import ClinicSerializer, DoctorSerializer, GallerySerializer
 
 
 class ClinicView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Clinic.objects.all()
     serializer_class = ClinicSerializer
 
 
 class DoctorView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
 
 
 class GalleryView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Gallery.objects.all()
     serializer_class = GallerySerializer
-
--- news/views.py
+++ news/views.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import News
 from .serializer import NewsSerializer
 
 
 class NewsView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = News.objects.all()
     serializer_class = NewsSerializer
