5106426a1d383b9a506de237d4b187311f569f60
TarasMkast <tarasmotalo@gmail.com>
2021-01-12 16:40:14 +0200

    fix admin

--- config/admin.py
+++ config/admin.py
@@ -3,14 +3,21 @@ from core.utils import CustomModelAdmin
 from config.models import *
 
 
-  
 @admin.register(Background)
 class BackgroundAdmin(CustomModelAdmin):
-    readonly_fields = ('background_photo',)
-    fields = ('background_photo', 'image', 'is_active')
+    readonly_fields = ('image',)
+    fields = ('image', 'is_active')
 
+    def has_add_permission(self, request):
+        if not self.model.objects.count():
+            return True
+        return False
 
 @admin.register(LicenseField)
 class LicenseFieldAdmin(CustomModelAdmin):
     fields = ('license_field',)
 
+    def has_add_permission(self, request):
+        if not self.model.objects.count():
+            return True
+        return False
